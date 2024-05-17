e3c37bb80aa95587bf2b6c879ccf93e826697eb2
Zaid <predcodes695@gmail.com>
2024-05-18 04:36:58 +0530

    add: jwt verification, test: userRoutes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI);
---
+++ backend/utils/authUtils.js
@@ -0,0 +1,49 @@
+const jwt = require('jsonwebtoken');
+
+const handleSignUpError = (err) => {
+    let error = {
+        username: "",
+        email: "",
+        password: "",
+    };
+    if (err.code === 11000){
+        if (err.keyValue.email){
+            error.email = "Email already taken";
+        }else if(err.keyValue.username){
+            error.username = "Username already taken";
+        }
+    }
+    else if(err.errors.username)
+        error.username = err.errors.username.message;
+    else if(err.errors.password)
+        error.password = err.errors.password.message
+    else if(err.errors.email)
+        error.email = "Please enter a valid email";
+    return error;
+}
+
+const handleLogInError = (err) => {
+    let errors = { username: '', password: '' };
+    if (err.message === 'User not found') {
+        errors.username = err.message;
+    } else if (err.message === 'Invalid password') {
+        errors.password = err.message;
+    }
+    return errors;
+}
+
+
+const maxAge = 86400; // 3 days in seconds
+const createToken = (username) => {
+    return jwt.sign(
+        { username : username },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: maxAge, },
+    );
+};
+
+module.exports = {
+    handleSignUpError,
+    handleLogInError,
+    createToken
+}
