eb34ba7dd1c716c708d5156cfad166b94868940f
Zaid <predcodes695@gmail.com>
2024-05-25 00:39:16 +0530

    add: new implementation of orderStore

--- frontend/src/components/Store/OrderStore.jsx
+++ frontend/src/components/Store/OrderStore.jsx
@@ -15,47 +15,6 @@ const useOrderStore = create((set) => ({
   pickupAddress: '',
   dropAddress: '',
 
-  isAuth: !!sessionStorage.getItem('isAuth'), // userCredentials
-  Phone: 0, // userCredentials
-  userName: sessionStorage.getItem('username'), // userCredentials
-  userEmail: '', // userCredentials
-  userRole: sessionStorage.getItem('userrole'), // userCredentials
-
-  addAuth: () => {
-    set((state) => {
-      return { ...state, isAuth: true };
-    });
-  },
-
-  removeAuth: () => {
-    set((state) => {
-      return { ...state, isAuth: false };
-    });
-  },
-
-  setUserPhone: (value) => {
-    set((state) => {
-      return { ...state, Phone: value };
-    });
-  },
-
-  setUserName: (value) => {
-    set((state) => {
-      return { ...state, userName: value };
-    });
-  },
-
-  setUserEmail: (value) => {
-    set((state) => {
-      return { ...state, userEmail: value };
-    });
-  },
-  setUserRole: (value) => {
-    set((state) => {
-      return { ...state, userRole: value };
-    });
-  },
-
   setPickupDate: (value) => {
     set((state) => {
       if (value === 'Today')
--- frontend/src/components/Store/OrderStore_.jsx
+++ frontend/src/components/Store/OrderStore_.jsx
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import moment from 'moment';
 import WashList from '../../TempData/WashList';
 import PowerCleanList from '../../TempData/PowerCleanList';
 import DryCleanList from '../../TempData/DryCleanList';
@@ -22,68 +23,178 @@ const useGeneralOrderStore = create((set) => ({
     powerCleanListTotal: initialState.powerCleanListTotal,
     dryCleanListTotal: initialState.dryCleanListTotal,
     orderTotal: 0,
+    pickupDate: '',
+    deliveryDate: '-- -- --',
+    pickupTime: '',
+    deliveryTime: '',
+    pickupAddress: '',
+    dropAddress: '',
   },
 
-  updateWashList: (newWashList) =>
-    set((state) => ({
-      order: {
-        ...state.order,
-        washList: newWashList,
-      },
-    })),
+  updateWashList: (newWashList) => {
+    set((state) => {
+      return {
+        order: {
+          ...state.order,
+          washList: newWashList,
+        },
+      };
+    });
+  },
 
   // Action to update PowerCleanList
-  updatePowerCleanList: (newPowerCleanList) =>
-    set((state) => ({
-      order: {
-        ...state.order,
-        powerCleanList: newPowerCleanList,
-      },
-    })),
+  updatePowerCleanList: (newPowerCleanList) => {
+    set((state) => {
+      return {
+        ...state,
+        order: {
+          ...state.order,
+          powerCleanList: newPowerCleanList,
+        },
+      };
+    });
+  },
 
   // Action to update DryCleanList
-  updateDryCleanList: (newDryCleanList) =>
-    set((state) => ({
-      order: {
-        ...state.order,
-        dryCleanList: newDryCleanList,
-      },
-    })),
+  updateDryCleanList: (newDryCleanList) => {
+    set((state) => {
+      return {
+        ...state,
+        order: {
+          ...state.order,
+          dryCleanList: newDryCleanList,
+        },
+      };
+    });
+  },
 
   // Action to update WashListTotal
-  updateWashListTotal: (newWashListTotal) =>
-    set((state) => ({
-      order: {
-        ...state.order,
-        washListTotal: newWashListTotal,
-      },
-    })),
+  updateWashListTotal: (newWashListTotal) => {
+    set((state) => {
+      return {
+        ...state,
+        order: {
+          ...state.order,
+          washListTotal: newWashListTotal,
+        },
+      };
+    });
+  },
 
   // Action to update PowerCleanListTotal
-  updatePowerCleanListTotal: (newPowerCleanListTotal) =>
-    set((state) => ({
-      order: {
-        ...state.order,
-        powerCleanListTotal: newPowerCleanListTotal,
-      },
-    })),
+  updatePowerCleanListTotal: (newPowerCleanListTotal) => {
+    set((state) => {
+      return {
+        ...state,
+        order: {
+          ...state.order,
+          powerCleanListTotal: newPowerCleanListTotal,
+        },
+      };
+    });
+  },
 
   // Action to update DryCleanListTotal
-  updateDryCleanListTotal: (newDryCleanListTotal) =>
-    set((state) => ({
-      order: {
-        ...state.order,
-        dryCleanListTotal: newDryCleanListTotal,
-      },
-    })),
+  updateDryCleanListTotal: (newDryCleanListTotal) => {
+    set((state) => {
+      return {
+        order: {
+          ...state.order,
+          dryCleanListTotal: newDryCleanListTotal,
+        },
+      };
+    });
+  },
 
   // Action to update OrderTotal
-  updateOrderTotal: (newOrderTotal) =>
-    set((state) => ({
-      order: {
-        ...state.order,
-        orderTotal: newOrderTotal,
-      },
-    })),
+  updateOrderTotal: (newOrderTotal) => {
+    set((state) => {
+      return {
+        order: {
+          ...state.order,
+          orderTotal: newOrderTotal,
+        },
+      };
+    });
+  },
+
+  // Action to update PickupDate and DeliveryDate
+  setPickupDate: (value) => {
+    set((state) => {
+      let newPickupDate;
+      let newDeliveryDate;
+
+      if (value === 'Today') {
+        newPickupDate = moment().format('Do MMM YYYY');
+        newDeliveryDate = moment().add(2, 'days').format('ddd, D MMM YYYY');
+      } else if (value === 'Tomorrow') {
+        newPickupDate = moment().add(1, 'days').format('ddd, D MMM YYYY');
+        newDeliveryDate = moment().add(3, 'days').format('ddd, D MMM YYYY');
+      } else {
+        newPickupDate = moment().add(2, 'days').format('ddd, D MMM YYYY');
+        newDeliveryDate = moment().add(4, 'days').format('ddd, D MMM YYYY');
+      }
+
+      return {
+        ...state,
+        order: {
+          ...state.order,
+          pickupDate: newPickupDate,
+          deliveryDate: newDeliveryDate,
+        },
+      };
+    });
+  },
+  // Action to update PickupTime
+  setPickupTime: (value) => {
+    set((state) => {
+      return {
+        ...state,
+        order: {
+          ...state.order,
+          pickupTime: value,
+        },
+      };
+    });
+  },
+
+  // Action to update DeliveryTime
+  setDeliveryTime: (value) => {
+    set((state) => {
+      return {
+        ...state,
+        order: {
+          ...state.order,
+          deliveryTime: value,
+        },
+      };
+    });
+  },
+
+  // Action to update PickupAddress
+  setPickupAddress: (value) => {
+    set((state) => {
+      return {
+        ...state,
+        order: {
+          ...state.order,
+          pickupAddress: value,
+        },
+      };
+    });
+  },
+
+  // Action to update DropAddress
+  setDropAddress: (value) => {
+    set((state) => {
+      return {
+        ...state,
+        order: {
+          ...state.order,
+          dropAddress: value,
+        },
+      };
+    });
+  },
 }));
 export default useGeneralOrderStore;
