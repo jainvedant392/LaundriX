21bf8096b4b6bf75766af4d0184af58b4e2e40e0
Zaid <predcodes695@gmail.com>
2024-05-25 17:01:39 +0530

    fix: fixed zustand state a bit

--- frontend/src/components/Store/OrderStore_.jsx
+++ frontend/src/components/Store/OrderStore_.jsx
@@ -1,111 +1,29 @@
 import { create } from 'zustand';
 import moment from 'moment';
-import WashList from '../../TempData/WashList';
-import PowerCleanList from '../../TempData/PowerCleanList';
-import DryCleanList from '../../TempData/DryCleanList';
-
-const initialState = {
-  washList: WashList,
-  powerCleanList: PowerCleanList,
-  dryCleanList: DryCleanList,
-  washListTotal: 0,
-  powerCleanListTotal: 0,
-  dryCleanListTotal: 0,
-};
 
 // Create the Zustand store
 const useGeneralOrderStore = create((set) => ({
   order: {
-    washList: initialState.washList,
-    powerCleanList: initialState.powerCleanList,
-    dryCleanList: initialState.dryCleanList,
-    washListTotal: initialState.washListTotal,
-    powerCleanListTotal: initialState.powerCleanListTotal,
-    dryCleanListTotal: initialState.dryCleanListTotal,
+    items: [],
     orderTotal: 0,
     pickupDate: '',
     deliveryDate: '-- -- --',
     pickupTime: '',
     deliveryTime: '',
     pickupAddress: '',
+
     deliveryAddress: '',
   },
-
-  updateWashList: (newWashList) => {
+  updateItems: (newItems) => {
     set((state) => {
       return {
         order: {
           ...state.order,
-          washList: newWashList,
+          items: newItems,
         },
       };
     });
   },
-
-  // Action to update PowerCleanList
-  updatePowerCleanList: (newPowerCleanList) => {
-    set((state) => {
-      return {
-        ...state,
-        order: {
-          ...state.order,
-          powerCleanList: newPowerCleanList,
-        },
-      };
-    });
-  },
-
-  // Action to update DryCleanList
-  updateDryCleanList: (newDryCleanList) => {
-    set((state) => {
-      return {
-        ...state,
-        order: {
-          ...state.order,
-          dryCleanList: newDryCleanList,
-        },
-      };
-    });
-  },
-
-  // Action to update WashListTotal
-  updateWashListTotal: (newWashListTotal) => {
-    set((state) => {
-      return {
-        ...state,
-        order: {
-          ...state.order,
-          washListTotal: newWashListTotal,
-        },
-      };
-    });
-  },
-
-  // Action to update PowerCleanListTotal
-  updatePowerCleanListTotal: (newPowerCleanListTotal) => {
-    set((state) => {
-      return {
-        ...state,
-        order: {
-          ...state.order,
-          powerCleanListTotal: newPowerCleanListTotal,
-        },
-      };
-    });
-  },
-
-  // Action to update DryCleanListTotal
-  updateDryCleanListTotal: (newDryCleanListTotal) => {
-    set((state) => {
-      return {
-        order: {
-          ...state.order,
-          dryCleanListTotal: newDryCleanListTotal,
-        },
-      };
-    });
-  },
-
   // Action to update OrderTotal
   updateOrderTotal: (newOrderTotal) => {
     set((state) => {
