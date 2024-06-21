8635fc632ae8bd5b2799b0de0d4f4eefe2f1382b
Zaid <predcodes695@gmail.com>
2024-06-21 11:03:52 +0530

    fix(frontend): fixed distortions in Landing Page

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
 import { Box, Center } from '@chakra-ui/react';
 import Hero from './Hero';
-import ContactSection from './Contact';
 import ServiceCard from './ServiceCard';
 import WorkingCard from './WorkingCard';
+import ContactSection from './Contact';
 
 function Main() {
   return (
     <Box
       pt="4rem"
       height="100vh"
-      // overflowY="scroll"
-      // scrollSnapType="y mandatory"
-      // css={{
-      //   scrollBehavior: 'smooth',
-      // }}
     >
-      <Center height="80vh">
+      <Box height="80vh">
         <Hero />
-      </Center>
-      <Center height="80vh">
+      </Box>
+      <Box height="80vh">
         <ServiceCard />
-      </Center>
-      <Center height="80vh">
+      </Box>
+      <Box height="100vh">
         <WorkingCard />
-      </Center>
+      </Box>
       <Center height="100vh">
         <ContactSection />
       </Center>
